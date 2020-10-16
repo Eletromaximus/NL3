9a9a73f5664d67a50ce51b1e91419c7739f1bc6a
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2020-10-16 17:32:41 -0300

    Quarto dia concluido

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import { View } from 'react-native'
+
+export default function OrphanageDetails () {
+  return <View />
+}
